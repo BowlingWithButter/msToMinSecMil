acd810b7283b62a3c59aac182ca65a95a35822d0
BowlingWithButter <36900163+BowlingWithButter@users.noreply.github.com>
2020-02-06 01:03:13 -0600

    added base file

---
+++ msToMinSecMil.java
@@ -0,0 +1,31 @@
+package randomWord;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author jrbas
+ * calculate given milliseconds into minutes, seconds and milliseconds
+ */
+public class msToMinSecMil {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double time = input.nextInt();
+		//System.out.println("The input is " + time);
+		double minutes = Math.floor(time * 0.0001 / 6);
+		time = time % (minutes * 60000);
+		System.out.println(minutes + " minute(s)");
+		//System.out.println("time left to calc = " + time);
+		double seconds = Math.floor(time * 0.001);
+		time = time % (minutes * 1000);
+		System.out.println(seconds + " second(s)");
+		//System.out.println("time left to calc = " + time);
+		double milliseconds = time;
+		time = time % time; // :p
+		System.out.println(milliseconds + " millisecond(s)");
+		//System.out.println("time left to calc = " + time);
+
+	}
+
+}
